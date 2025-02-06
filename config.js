d729451d6846ed8a4eaccfc33da5958ab6963784
Martin Rodriguez <martinrodfl@gmail.com>
2025-02-06 17:40:18 -0300

    Another commit

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-export const { PORT = 3000 } = process.env;
+// process.loadEnvFile();
+
+// export const {
+//   PORT = process.env.PORT,
+//   DB_HOST = process.env.DB_HOST,
+//   DB_USER = process.env.DB_USER,
+//   DB_PASS = process.env.DB_PASS,
+//   DB_PORT = process.env.DB_PORT,
+// } = process.env;
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-import app from "./src/app.js";
-import { PORT } from "./config.js";
+// config.mjs
+import { loadEnvFile } from "node:process";
+loadEnvFile();
 
-console.log(PORT);
+console.log("PORT", process.env.PORT); // Verifica que la variable PORT se haya cargado
+
+import app from "./src/app.js";
+
+const PORT = process.env.PORT || 3000; // Valor por defecto en caso de que PORT no esté definido
+console.log("PORT", PORT);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hi pipollllllllllll</h1>");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server start on port ${PORT}`);
-  console.log(process.env.PORT);
+  console.log(`Server started on port ${PORT}`);
+  console.log(PORT);
 });
